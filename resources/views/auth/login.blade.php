f3135f8e4a27cd5a09ff88cb1b95cc0700225548
Prabhat Kumar <info.developer.email@gmail.com>
2024-04-26 16:23:31 +0530

    take code from ftp

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,46 +1,82 @@
 <html>
 	<header>
-		<title>Essay Help Login Form</title>
+		<title>500m Login Form</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-        <link rel="stylesheet" href="{{ asset('css/login.css') }}">
-        
+        <link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+        <style>
+			
+			.error{
+				color:#DE3545 !important;
+				width:100%;
+				font-weight:normal !important;
+			}
+			
+			input.error{
+				border-color:#DE3545 !important;
+			}
+			.login-card-body .input-group .form-control
+			{
+				border:1px solid #d9d1d1 !important;
+				border-radius:5px !important;
+			}
+			
+		</style>
 	</header>
-	<body>
-		<div class="wrapper">
-			<?php /*<div class="logo">
-				<img src="https://www.freepnglogos.com/uploads/twitter-logo-png/twitter-bird-symbols-png-logo-0.png" alt="">
-			</div>*/?>
-			<div class="text-center mt-4 name">
-				Essay Help
-			</div>
-            <x-auth-session-status class="mb-4" :status="session('status')" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2 error email-error" />
-			<form class="p-3 mt-3" id="login_form" method="POST"  action="{{ route('login') }}">
-                @csrf
-				<div class="form-field d-flex align-items-center">
-					<span class="far fa-user"></span>
-					<input type="text" name="email" id="email" placeholder="Email">
-                    
-				</div>
-				<div class="form-field d-flex align-items-center">
-					<span class="fas fa-key"></span>
-					<input type="password" name="password" id="password" placeholder="Password">
-                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
-				</div>
-				<button class="btn mt-3">Login</button>
-			</form>
-			<?php /*<div class="text-center fs-6">
-				<a href="#">Forget password?</a> or <a href="#">Sign up</a>
-			</div>*/?>
-		</div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<body class="hold-transition login-page" style="background-image: url('images/1.jpg');">
+		<div class="login-box">
+  <div class="login-logo">
+    <a href="index.html"><b>Wecome to </b>admin login</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card" style="border-radius: 1rem; ">
+    <div class="card-body login-card-body" style="border-radius: 1rem; ">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form id="login_form" method="POST"  action="{{ route('login') }}">
+	    @csrf
+	    <x-auth-session-status class="mb-4" :status="session('status')" />
+        
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+		 <x-input-error :messages="$errors->get('email')" class="mt-2 error" />
+          
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+          
+		  <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+		
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/jquery.validate.min.js"></script>
 		<script src="https://cdn.jsdelivr.net/jquery.validation/1.16.0/additional-methods.min.js"></script>
 		<script>
-			$().ready(function () {
+			$(document).ready(function () {
  
 				$("#login_form").validate({
 			 
@@ -59,8 +95,6 @@
 				});
 			});
 		</script>
-        <style>
-			
-		</style>
+        
 	</body>
 </html>
